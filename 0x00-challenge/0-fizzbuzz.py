5fa71d56e296a2a58b3c3b97ed0e0fb835666fb2
LabyKreative <labykreative@yahoo.com>
2023-07-16 13:00:17 +0100

    FizzBuzz

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""FizzBuzz function"""
+import sys
+
+
+def fizzbuzz(n):
+    """FizzBuzz function"""
+    if n < 1:
+        print("n is less than 1")
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+    print("FizzBuzz is done")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
+    print("End of FizzBuzz")
